acc5bc3165833a37ff02b2395437747d38980790
Zhenkaaf <zhenkaaf@gmail.com>
2026-03-09 18:02:18 +0200

    version-2

--- src/store/useCounter.tsx
+++ src/store/useCounter.tsx
@@ -1,22 +1,25 @@
-import { create } from "zustand";
+import { create, type StateCreator } from "zustand";
 
-interface ICounterState {
-    value: number;
+interface IActions {
     increaseCount: () => void;
     decreaseCount: () => void;
 }
 interface IInitialState {
     value: number;
 }
+interface ICounterState extends IInitialState, IActions {}
 
 const initialState: IInitialState = {
     value: 66,
 };
-export const useCounter = create<ICounterState>()((set) => ({
+
+const counterStore: StateCreator<ICounterState> = (set) => ({
     ...initialState,
     increaseCount: () => set((state) => ({ value: state.value + 1 })),
     decreaseCount: () => set((state) => ({ value: state.value - 1 })),
-}));
+});
+
+export const useCounter = create<ICounterState>()(counterStore);
 
 export const useGetCount = () => useCounter((state) => state.value);
 export const increaseCountFn = () => useCounter.getState().increaseCount();
